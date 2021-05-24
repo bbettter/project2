6ad99c92a923ec5953d1638442223b96c237e121
bbettter <bbetttter@gmail.com>
2021-05-24 20:03:54 +0300

    practice1

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const container = document.querySelector('.container').addEventListener('click', event => {
-    if (event.target.classList('textcenter')){
-        event.target.style.color = 'red'
-    }
-})
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','')
+
+const personalMovieDB = {
+    
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    privat: false
+}
+const a1 = prompt('Какой последний фильм вы смотрели?','')
+const a2 = +prompt('На сколько оцените фильм','')
+const a3 = prompt('Какой последний фильм вы смотрели?','')
+const a4 = +prompt('На сколько оцените фильм','')
+
+personalMovieDB.movies[a1] = a2
+personalMovieDB.movies[a3] = a4
+console.log(personalMovieDB)
